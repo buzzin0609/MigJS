0077220e4bb991e6bc65092fd35d982395fcb9f9
William Busby <William.Busby@haynet.com>
2016-12-15 17:23:32 +0000

    more comments in main mig file

--- mig.js
+++ mig.js
@@ -53,18 +53,35 @@
         return arr.push.apply(thisArg, els);
     }
 
+    /**
+     * The main Mig function
+     * @param {Mixed} selector either a CSS string selector or DOM element(s)
+     * @param {undefined} els placeholder variable for the future DOM array
+     */
     function Mig(selector, els) {
         els = '' + selector === selector && _lookup(selector) || selector[0] && selector || [selector];
         return _init(this, els);
     }
+
+    /**
+     * The selector function exposed to the global scope. Takes the same selector param as above but returns a new instance of Mig
+     */
     win.m = function(a) {
         return new Mig(a);
     };
+
+    //This is key, it adds all Array prototype functions and acts like an array instead of an object. You can still assign functions to the prototype like an object. 
     Mig.prototype = [];
 
+    /**
+     * The main extension function
+     */
     Mig.prototype.extend = function(name, fn) {
         Mig.prototype[name] = fn;
     };
 
+    /**
+     * Create a static Object of Mig so static functions can be used without having to select an element. Useful when registering global events with Mig.on('event', callback); and then Mig.on.trigger('event');
+     */
     win.Mig = Object.create(Mig.prototype);
 }(window, document, []));
